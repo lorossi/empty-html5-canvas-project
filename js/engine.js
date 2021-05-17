fccd930e8c6a3d123a941349c287dfff962535d7
lorossi <contact@lorenzoros.si>
2021-05-17 22:52:17 +0200

    added colors class

--- js/engine.js
+++ js/engine.js
@@ -212,3 +212,41 @@ document.addEventListener("DOMContentLoaded", () => {
   // keyboard event listeners
   document.addEventListener("keydown", e => s.keydown(e));
 });
+
+class Color {
+  constructor(r, g, b, a = 1) {
+    this._r = r;
+    this._g = g;
+    this._b = b;
+    this._a = a;
+  }
+
+  get alpha() {
+    return this._a;
+  }
+
+  set alpha(a) {
+    this._a = a;
+  }
+
+  get rgb() {
+    return `rgb(${this._r}, ${this._g}, ${this._b})`;
+  }
+
+  get rgba() {
+    return `rgba(${this._r}, ${this._g}, ${this._b}, ${this._a})`;
+  }
+
+  get channel() {
+    return {
+      monochromatic: this._r == this._g && this._g == this._b,
+      channel: this._r,
+    };
+  }
+
+  set channel(s) {
+    this._r = s;
+    this._g = s;
+    this._b = s;
+  }
+}
